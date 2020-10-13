168ff2703d705e1f6a3cdb7aad798ee5d1152a05
横尾杏之介 <yokoo.an209@gmail.com>
2020-10-13 11:48:25 +0900

    first_app

---
+++ Rails_app/first_app/app/controllers/boards_controller.rb
@@ -0,0 +1,10 @@
+class BoardsController < ApplicationController
+    def index
+
+    end
+
+    def new
+        
+    end
+    
+end
---
+++ Rails_app/first_app/config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "boards" => "boards#index"
+  get "boards/new" => "boards#new"
+
+end
