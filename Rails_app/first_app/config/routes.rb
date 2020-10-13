18569fdcbba268b6739066958537665a81af2de7
横尾杏之介 <yokoo.an209@gmail.com>
2020-10-13 21:56:43 +0900

    初めてのapp完成

--- Rails_app/first_app/config/routes.rb
+++ Rails_app/first_app/config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :boards do
+    resources :comments, only: [:create, :destroy]
+  end
+
+  root to: "boards#index"
+
   get "boards" => "boards#index"
   get "boards/new" => "boards#new"
+  post "boards" => "boards#create"
+  get "boards/:id" => "boards#show"
+  get "boards/:id/edit" => "boards#edit"
+  put "boards/:id" => "boards#update" #put は全部を更新したい時のメソッド
+  delete "boards/:id" => "boards#destroy"
+
 
 end
